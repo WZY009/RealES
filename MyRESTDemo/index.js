73e80da0c4baf007c239633e606bf90236636170
WZY009 <2232213966@qq.com>
2022-04-25 11:56:09 +0800

    Finishing Section35-Defining RESTful Routes

--- MyRESTDemo/index.js
+++ MyRESTDemo/index.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const { v4 : uuid } = require('uuid');
+const methodOverride = require('method-override')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
+// What is the function of method override in express
+// Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 
-const comments = [
+let comments = [
     {
-        id:1,
+        id:uuid(),
         username:"Trry",
         comment:"Funny"
     },
     {
-        id:2,
+        id:uuid(),
         username:"Tommy",
         comment:"That's a fucking crazy plan!"
+    },
+    {
+        id:uuid(),
+        username:"Jerry",
+        comment:"Come on!"
+    },
+    {
+        id:uuid(),
+        username:"Mary",
+        comment:"Why you are still unable to find an intern?"
     }
 ]
 
@@ -28,14 +43,32 @@ app.get('/comments/new', (req, res)=>{
 })
 app.post('/comments', (req, res)=>{
     const {username, comment} = req.body
-    comments.push({username, comment})
+    comments.push({username, comment, id:uuid()})
     res.redirect('/comments')
+})
+app.get('/comments/:id/edit', (req, res)=>{
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', {comment})
 })
 app.get('/comments/:id', (req, res)=>{
     const {id} = req.params
-    const comment = comments.find(c => c.id === parseInt(id))
+    const comment = comments.find(c => c.id === id)
     res.render('comments/show', {comment})
 }) 
+app.delete('/comments/:id', (req, res)=>{
+    const {id} = req.params
+    comments = comments.filter(c =>c.id !== id) // 将除了非目标的comment全部保留
+    res.redirect('/comments')
+})
+app.patch('/comments/:id', (req, res)=>{
+    const {id} = req.params
+    const newCommentText = req.body.comment
+    const foundComment = comments.find(c => c.id === id)
+    foundComment.comment = newCommentText
+    res.redirect('/comments')
+
+})
 app.get('/taco', (req, res)=>{
     res.send("Get response")
 })
