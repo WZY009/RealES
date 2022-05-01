0aa5ab271d7854acf138db321067845a860b1716
WZY009 <2232213966@qq.com>
2022-05-01 23:12:43 +0800

    section 39

---
+++ MyYelpCamp/app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp') 
+
+const app = express();
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", ()=>{
+    console.log("Database connected")
+})
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+app.get('/campgrounds', async (req, res)=>{
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index',{campgrounds})
+})
+app.get('/campgrounds/new', (req, res)=>{
+    res.render('campgrounds/new')
+}) 
+// pay attention！ the order is crucial because if this get is behind '/campgrounds/:id', new will be identified as id
+app.post('/campgrounds', async(req, res)=>{
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save()
+    res.redirect(`/campgrounds/${newCampground._id}`)
+})
+app.get('/campgrounds/:id/edit', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
+app.put('/campgrounds/:id', async(req, res)=>{
+    const {id} = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+app.delete('/campgrounds/:id', async(req, res)=>{
+    const {id} = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
+app.get('/campgrounds/:id', async(req, res)=>{
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
+})
+
+app.listen(3000, ()=>{
+    console.log('Serving on the host 3000!')
+})
