d9331f4cac7926b87671248881d855c2ea3818ff
WZY009 <2232213966@qq.com>
2022-05-05 22:49:58 +0800

    Section 41

--- MyYelpCamp/app.js
+++ MyYelpCamp/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
-
+const ejsMate = require('ejs-mate')
 mongoose.connect('mongodb://localhost:27017/yelp-camp') 
 
 const app = express();
@@ -15,6 +15,7 @@ db.once("open", ()=>{
     console.log("Database connected")
 })
 
+app.engine('ejs', ejsMate) 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
